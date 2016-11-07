321a9cdc678bfd28e7cbd76753bc462fa793eefd
Diallo Hudgins <d@diallohudgins.com>
2016-11-06 22:06:49 -0500

    added preloader

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -32,13 +32,15 @@ window.onload = convertMailAddress;
 
 (function($) {
 
-	$(function(){
+	$(function() {
+
+		// Init ScrollMagic
+    	var controller = new ScrollMagic.Controller();
 
 		// Foundation init
 		$(document).foundation();
 
 		// hamburger
-		// hambuger
 	    function hamburgerNav() {
 	    	var $hb = $('.hamburger');
 	    	$hb.on('click', function(e){
@@ -189,6 +191,80 @@ window.onload = convertMailAddress;
 
 		}
 		secondaryNav();
+
+
+		// preload
+		function preLoad() {
+			// get all slides
+			var slides = ["#slide01", "#slide02", "#slide03"];
+
+			// get all headers in slides that trigger animation
+			var headers = ["#slide01 header", "#slide02 header", "#slide03 header"];
+
+			// get all break up sections
+			// var breakSections = ["#cb01", "#cb02", "#cb03"];
+
+			// number of loaded images for preloader progress 
+			var loadedCount = 0; //current number of images loaded
+			var imagesToLoad = $('.bcg').length; //number of slides with .bcg container
+			var loadingProgress = 0; //timeline progress - starts at 0
+
+			$('.bcg').imagesLoaded({
+			    background: true
+			}
+			
+			).progress( function( instance, image ) {
+				loadProgress();
+			});
+
+			function loadProgress(imgLoad, image) {
+			 	//one more image has been loaded
+				loadedCount++;
+
+				loadingProgress = (loadedCount/imagesToLoad);
+
+				//console.log(loadingProgress);
+
+				// GSAP timeline for our progress bar
+				TweenLite.to(progressTl, 0.7, {progress:loadingProgress, ease:Linear.easeNone});
+
+			}
+
+			//progress animation instance. the instance's time is irrelevant, can be anything but 0 to void  immediate render
+			var progressTl = new TimelineMax({paused:true,onUpdate:progressUpdate,onComplete:loadComplete});
+
+			//tween the progress bar width
+			progressTl.to($('.progress span'), 1, {width:100, ease:Linear.easeNone});
+
+			//as the progress bar witdh updates and grows we put the precentage loaded in the screen
+			function progressUpdate() {
+				//the percentage loaded based on the tween's progress
+				loadingProgress = Math.round(progressTl.progress() * 100);
+				//we put the percentage in the screen
+				$(".txt-perc").text(loadingProgress + '%');
+
+			}
+
+			function loadComplete() {
+
+				// preloader out
+				var preloaderOutTl = new TimelineMax();
+
+				preloaderOutTl
+					.to($('.progress'), 0.3, {y: 100, autoAlpha: 0, ease:Back.easeIn})
+					.to($('.txt-perc'), 0.3, {y: 100, autoAlpha: 0, ease:Back.easeIn}, 0.1)
+					.set($('body'), {className: '-=is-loading'})
+					.set($('#intro'), {className: '+=is-loaded'})
+					.to($('#preloader'), 0.7, {yPercent: 100, ease:Power4.easeInOut})
+					.set($('#preloader'), {className: '+=is-hidden'})
+					.from($('#intro .title'), 1, {autoAlpha: 0, ease:Power1.easeOut}, '-=0.2')
+					.from($('#intro p'), 0.7, {autoAlpha: 0, ease:Power1.easeOut}, '+=0.2')
+					.from($('.scroll-hint'), 0.3, {y: -20, autoAlpha: 0, ease:Power1.easeOut}, '+=0.1');
+
+				return preloaderOutTl;
+			}
+		}
+		preLoad();
 
 		
 
